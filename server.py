17111b3486c724644b1d0d384c495f36a6ee3816
filipzabic <filip.zabic@gmail.com>
2022-02-27 09:57:28 +0100

    Minor template structure change

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from flask import render_template as template
 
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+route = app.route
+
+
+@route('/')
+def index():
+    return template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
